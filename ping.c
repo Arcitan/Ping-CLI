66c3f1e678ed399d6f8e60df722c7affa0728a0c
Franklin Zhang <franklinzgma@gmail.com>
2020-04-22 20:48:43 -0500

    implemented infinite ping loop

--- ping.c
+++ ping.c
@@ -1,5 +1,4 @@
 /*
- *
  * A simple "ping" CLI application for Unix-based systems. Accepts a
  * hostname/IP address from the command line, then sends ICMP "echo requests"
  * in a loop to the target while receiving "echo reply" messages. Reports
@@ -12,6 +11,7 @@
  *   (4) https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol
  *   (5) https://opensourceforu.com/2015/03/a-guide-to-using-raw-sockets/
  *   (6) https://www.geeksforgeeks.org/internet-control-message-protocol-icmp/
+ *
  */
 #include <sys/time.h>
 #include <sys/types.h>
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <arpa/inet.h>
@@ -38,6 +39,7 @@
 #define MAXLINE         8192
 #define PACKETSIZE      64
 #define PAYLOADSIZE     (PACKETSIZE - sizeof(struct icmphdr))
+#define SLEEPRATE       1
 #define TTLVAL          255
 
 /* Structs */
@@ -51,11 +53,12 @@ struct protoent *proto = NULL;  /* Pointer to protocol info */
 struct sockaddr_in serveraddr;  /* The destination IP info */
 int pid = -1;                   /* Process ID of this process */
 int sd;                         /* Socket file descriptor */
+bool pingflag = 1;              /* Flag for the infinite ping loop */
 
 /* Function signatures */
 static int open_socket(const char *hostname);
 
-static void ping();
+static void ping(const char *hostname, const char *ip);
 
 /*
  * Requires:
@@ -83,49 +86,88 @@ unsigned short checksum(void *b, int len)
 
 /*
  * Requires:
- *   None.
+ *   "hostname" must be a valid NUL-terminated host name string.
+ *   "ip" must be a valid NUL-terminated IP address.
  *
  * Effects:
  *   Pings the destination server given by "hostname" with an ICMP packet.
  */
 static void
-ping()
+ping(const char *hostname, const char *ip)
 {
         struct sockaddr_in clientaddr;
         struct packet pckt;
+        struct timespec ts, te;
+        long double pckt_time, rtt;
         unsigned int clientlen = sizeof(clientaddr);
         unsigned int i;
-        int message_count = 1;
+        int sent_cnt = 0, received_cnt = 0;
+        bool was_sent = 1;
 
-        /* Create the ICMP packet */
-        bzero(&pckt, sizeof(pckt));
-        pckt.hdr.type = ICMP_ECHO;
-        pckt.hdr.un.echo.id = pid;
-        for (i = 0; i < sizeof(pckt.payload) - 1; i++)
-                pckt.payload[i] = i + '0';
-        pckt.payload[i] = 0;
-        pckt.hdr.un.echo.sequence = message_count++;
-        pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
 
-        /* Send the packet */
-        if (sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *) &serveraddr,
-            sizeof(serveraddr)) <= 0)
-                perror("sendto");
-        sleep(1);
+        while (pingflag) {
+                /* Create the ICMP packet */
+                bzero(&pckt, sizeof(pckt));
+                pckt.hdr.type = ICMP_ECHO;
+                pckt.hdr.un.echo.id = pid;
+                for (i = 0; i < sizeof(pckt.payload) - 1; i++)
+                        pckt.payload[i] = i + '0';
+                pckt.payload[i] = 0;
+                pckt.hdr.un.echo.sequence = sent_cnt++;
+                pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
 
-        /*
-         * Receive the returning packet from the socket. Check for recvfrom
-         * errors if we've sent at least one message, since we expect a
-         * response.
-         */
-        if ((recvfrom(sd, &pckt, sizeof(pckt), 0,
-            (struct sockaddr *) &clientaddr, &clientlen) < 0) &&
-            message_count > 1) {
-                /* If we've been interrupted by a signal, ignore the error */
-                if (!(errno = EINTR)) {
-                        perror("ping: recvfrom");
+                sleep(SLEEPRATE);
+
+                /* Send the packet */
+                clock_gettime(CLOCK_MONOTONIC, &ts);
+                if (sendto(sd, &pckt, sizeof(pckt), 0,
+                    (struct sockaddr *) &serveraddr,
+                    sizeof(serveraddr)) <= 0) {
+                        perror("sendto");
+                        // Mark packet as not sent
+                        was_sent = 0;
+                }
+
+                /* Receive the returning packet */
+                if ((recvfrom(sd, &pckt, sizeof(pckt), 0,
+                    (struct sockaddr *) &clientaddr, &clientlen) < 0)) {
+                        /*
+                         * Ignore any signal interrupt errors, since that's
+                         * part of normal usage (user will CTRL+C)
+                         */
+                        if (!(errno = EINTR)) {
+                                perror("ping: recvfrom");
+                        }
+
+                        // Try next packet since we failed to receive this one
+                        continue;
+                }
+
+                /* Compute elapsed time between sending and receiving */
+                clock_gettime(CLOCK_MONOTONIC, &te);
+                pckt_time = ((double) (te.tv_nsec - ts.tv_nsec)) /
+                    1000000.0;
+                rtt = (te.tv_sec - ts.tv_sec) * 1000.0 + pckt_time;
+
+                /* Inspect the received packet and print out results */
+                if (was_sent) {
+                        // Verify packet consistency
+                        if (pckt.hdr.code != ICMP_ECHOREPLY) {
+                                fprintf(stderr,
+                                    "packet received with ICMP type %d and code "
+                                    "%d\n", pckt.hdr.type, pckt.hdr.code);
+                        }
+
+                        // Print results
+                        printf(
+                            "%d bytes from %s (%s): icmp_seq=%d ttl=%d rtt=%Lf "
+                            "ms\n", PACKETSIZE, hostname, ip, sent_cnt, TTLVAL,
+                            rtt);
+
+                        received_cnt++;
                 }
         }
+
 }
 
 
@@ -184,7 +226,7 @@ main(int argc, char **argv)
         struct addrinfo *ai;
         const int ttlval = TTLVAL;
         int err;
-        char *host;
+        char *host, *ip;
 
         /* Verify usage */
         if (argc != 2) {
@@ -232,7 +274,8 @@ main(int argc, char **argv)
                 perror("setsockopt: failed to set TTL value");
 
         // Ping the host
-        ping();
+        ip = inet_ntoa(serveraddr.sin_addr);
+        ping(host, ip);
 
         return (0);
 }
